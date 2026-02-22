f82d1326c9e6187b84b4f9f987096785500d6b83
Armaan1804 <airman.atri@gmail.com>
2026-02-22 17:23:04 +0530

    feat: implement authentication logic and user model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,107 @@
-export const signup = (req, res) => {
-    res.send("Signup");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const user = await User.findOne({ email });
+
+        if (user) return res.status(400).json({ message: "Email Already Exists" });
+
+        // hashing the password
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword
+        })
+
+        if (newUser) {
+
+            // Generating the JWT token
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
+        } else {
+            res.status(400).json({ message: "Invalid User Data" });
+        }
+    } catch (error) {
+        // Displaying the Error MEssage for the Controller
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+
+    }
 };
 
-export const login = (req, res) => {
-    res.send("Login");
+export const login = async (req, res) => {
+
+    // Destructuring the data from the request body that is email and password
+    const { email, password } = req.body;
+
+    // Checking if the user exists in the database
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        //Comparing the password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        // Generating the JWT Token
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+
+
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+
+
+
+
 };
 
 export const logout = (req, res) => {
-    res.send("Logout");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out sucessfully" });
+
+    } catch (error) {
+        console.log("Error in the logout controller", error.message);
+        res.status(500).json({ message: " Internal Server Error" });
+
+    }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// Middleware
+app.use(express.json());
+
 // Path for Routes
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
